5609b66febdf84f86296db76bfaa2a375457afa0
Masayuki Ushio <ong8181@gmail.com>
2021-10-30 12:03:41 +0900

    Update 01_UsefulRCodes.R

--- 06_ToolBox/01_UsefulRCodes.R
+++ 06_ToolBox/01_UsefulRCodes.R
@@ -9,3 +9,31 @@
 od_name <- basename(rstudioapi::getSourceEditorContext()$path)
 (output_folder <- paste0(str_sub(od_name, end = -3), "Out")); rm(od_name)
 dir.create(output_folder)
+
+
+# ----------------------------------------- #
+# tidyverse
+# ----------------------------------------- #
+# Change the number of rows/columns to be displayed 
+options(tibble.print_min = 20)
+options(tibble.width = Inf)
+
+
+# ----------------------------------------- #
+# ggplot2 tools
+# ----------------------------------------- #
+# Label exponents in ggplot
+label_func <- function(x) {
+  ifelse(x == 0, "0", 
+         parse(text = gsub("[+]", "", gsub("e", " %*% 10^", scales::scientific_format()(x))))
+         )
+  }
+
+# Create color palette
+library(RColorBrewer)
+get_palette <- colorRampPalette(brewer.pal(8, "Paired"))
+get_palette(20)
+
+# Add new line in expression()
+g1 + xlab(expression(atop(paste("Water temperature (", degree, "C)"), "(Maximum value)")))
+
